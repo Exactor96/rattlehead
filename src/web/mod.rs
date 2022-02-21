a4e3b52163f8bf65ff35bef11cc0467ecde1b2d6
Maksim Kuznetsov <maksim.kuznetsov@akvelon.com>
2022-02-21 20:26:18 +0300

    updating IP

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -4,5 +4,5 @@ pub async fn start_web() {
     // Match any request and return hello world!
     let routes = warp::any().map(|| "Hello, World!");
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
