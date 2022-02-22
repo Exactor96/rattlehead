94b32800cf1b183d11d6be15ff85569dc8e9ea23
Maksim Kuznetsov <maksim.kuznetsov@akvelon.com>
2022-02-22 23:24:33 +0300

    updating running web only

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,11 +1,33 @@
 use warp::Filter;
-use pretty_env_logger;
 
-pub async fn start_web() {
-    pretty_env_logger::init();
-    println!("WEB STARTED");
-    // Match any request and return hello world!
-    let routes = warp::any().map(|| "Hello, World!");
+pub async fn start_web() -> Result<(), Box<dyn std::error::Error>> {
+    // Heroku injects the port as an environment variable
+    let port = std::env::var("PORT")
+        .ok()
+        .map(|val| val.parse::<u16>())
+        .unwrap_or(Ok(8080))?;
 
-    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
+    // GET /hello
+    let hello = warp::path("hello").and(warp::get()).map(|| "World");
+
+    // GET /world
+    let world = warp::path("world").and(warp::get()).map(|| "Hello");
+
+    // fallback
+    let fallback = warp::any().map(|| "Not found");
+
+    let routes = hello.or(world).or(fallback);
+
+    println!("Starting server on port: {}", port);
+    let (_addr, server) =
+        warp::serve(routes).bind_with_graceful_shutdown(([0, 0, 0, 0], port), async {
+            tokio::signal::ctrl_c()
+                .await
+                .expect("http_server: Failed to listen for CRTL+c");
+            println!("Shutting down HTTP server");
+        });
+
+    server.await;
+    Ok(())
+
 }
