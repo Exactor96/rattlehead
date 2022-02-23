2ca15779bbeb7f1ed8d5b5232ac7859b6e311ec2
Maksim Kuznetsov <maksim.kuznetsov@akvelon.com>
2022-02-23 04:07:37 +0300

    checking actix-web for heroku

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
-use std::env;
-use std::process::exit;
+use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use env_logger;
 
-mod bot;
-mod web;
+async fn index(req: HttpRequest) -> &'static str {
+    println!("REQ: {:?}", req);
+    "Hello world!"
+}
 
-#[tokio::main]
-async fn main() {
-    let args: Vec<String> = env::args().collect();
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
 
-    if &args[1] == "bot" {
-        bot::start_bot().await;
-    } else if &args[1] == "web" {
-        web::start_web().await;
-    }
-    {
-        exit(1);
-    }
+    HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/index.html").to(|| async { "<h1>Hello world!</h1>" }))
+            .service(web::resource("/").to(index))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
